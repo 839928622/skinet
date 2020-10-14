7d7931b3776fa6ecb2dbb93879ebdb6aac8c4bc8
Parker <jwk839928622@outlook.com>
2020-10-14 21:23:31 +0800

    first commit

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,24 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+       
+
+        public StoreContext()
+        {
+            
+        }
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+           
+        }
+
+       
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
