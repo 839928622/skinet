47a3fad7286706a8854930f05bc3ee3bb7f21459
Parker <jwk839928622@outlook.com>
2020-10-18 21:35:55 +0800

    Generic parttern and specification parttern

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Core.Entities;
 using Core.Interfaces;
+using Core.Specifications;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,29 +15,40 @@ namespace API.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IProductRepos _productRepos;
+        private readonly IGenericRepository<Product> _productRepo;
+        private readonly IGenericRepository<ProductBrand> _productBrandRepo;
+        private readonly IGenericRepository<ProductType> _productTypeRepo;
 
-        public ProductsController(IProductRepos productRepos)
+        public ProductsController(IProductRepos productRepos,
+            IGenericRepository<Product> productRepo,
+            IGenericRepository<ProductBrand> productBrandRepo,
+            IGenericRepository<ProductType> productTypeRepo)
         {
             _productRepos = productRepos;
+            _productRepo = productRepo;
+            _productBrandRepo = productBrandRepo;
+            _productTypeRepo = productTypeRepo;
         }
 
-        [HttpGet]
-        public async  Task<ActionResult<List<Product>>> GetProductsAsync() => Ok(await _productRepos.GetProductsAsync()) ;
+        [HttpGet("products")]
+        public async  Task<ActionResult<List<Product>>> GetProductsAsync() =>
+            Ok(await _productRepo.ListAsync(new ProductsWithTypesAndBrandsSpecification())) ;
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Product>> GetProduct(int id) => Ok(await _productRepos.GetProductByIdAsync(id));
+        public async Task<ActionResult<Product>> GetProduct(int id) => 
+            Ok(await _productRepo.ListAsync(new ProductsWithTypesAndBrandsSpecification(id)));
 
         [HttpGet("brands")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
         {
-         return   Ok(await _productRepos.GetProductBrandsAsync());
+         return   Ok(await _productBrandRepo.ListAllAsync());
         }
 
 
         [HttpGet("types")]
         public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductTypes()
         {
-            return Ok(await _productRepos.GetProductTypesAsync());
+            return Ok(await _productTypeRepo.ListAllAsync());
         }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -23,6 +23,7 @@ namespace API
         {
             services.AddControllers();
             services.AddScoped<IProductRepos, ProductRepository>();
+            services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
             services.AddDbContext<StoreContext>(options =>
             {
                 options.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
