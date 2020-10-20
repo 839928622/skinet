ef328d798e1d2a5cdd590a6fbeda1edfec1bc104
Parker <jwk839928622@outlook.com>
2020-10-20 20:32:59 +0800

    custom value resolver for automapper

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -12,7 +12,8 @@ namespace API.Helpers
                 .ForMember(d=>d.ProductBrand,
                               o=>o.MapFrom(s => s.ProductBrand.Name))
                 .ForMember(d => d.ProductType,
-                    o=> o.MapFrom(p=>p.ProductType.Name));
+                    o=> o.MapFrom(p=>p.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o=> o.MapFrom<ProductUrlResolver>()); // for url resolver
         }
     }
 }
---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,27 @@
+﻿using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product,ProductToReturnDto,string>
+    {
+        private readonly IConfiguration _configuration;
+
+        public ProductUrlResolver(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        /// <inheritdoc />
+        public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _configuration.GetSection("ApiUrl") + source.PictureUrl; // _configuration["ApiUrl"]
+            }
+
+            return null;
+        }
+    }
+}
