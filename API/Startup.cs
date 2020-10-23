748dc932e42972746ce903f17c4011d91aca54c5
Parker <jwk839928622@outlook.com>
2020-10-23 20:54:28 +0800

    bring in swagger and configure it

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -53,6 +54,11 @@ namespace API
                     return new  BadRequestObjectResult(errorResponse);// new response
                 };
             });
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",new OpenApiInfo(){Title = "Skinet API",Version = "V1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,6 +80,11 @@ namespace API
 
             app.UseAuthorization();
 
+            app.UseSwagger();
+            app.UseSwaggerUI(x =>
+            {
+                x.SwaggerEndpoint("/swagger/v1/swagger.json", "SKiNet APi v1");
+            });
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
