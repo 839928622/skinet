f80e9b6d607c975ff9ee866f6b40a1acc992134e
Parker <jwk839928622@outlook.com>
2020-10-31 14:09:44 +0800

    resolve bug:  pageNumber need to be set to 1 while searching、filtering products

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -54,11 +54,13 @@ sortOptions = [{name: 'Aplhabetical', value: 'name'}, {name: 'Price: Low to High
 
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -76,6 +78,7 @@ sortOptions = [{name: 'Aplhabetical', value: 'name'}, {name: 'Price: Low to High
 
   onSearch() {
     this.shopParams.search = this.searchTerm.nativeElement.value;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
