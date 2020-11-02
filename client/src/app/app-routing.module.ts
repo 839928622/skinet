1a40a24073a6d35b1c885e5953af5fc79abe837a
Parker <jwk839928622@outlook.com>
2020-11-02 20:39:36 +0800

    create two error components (404 and 500)  for error interceptor redirect to .

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ShopComponent } from './shop/shop.component';
-import { ProductDetailComponent } from './shop/product-detail/product-detail.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: 'not-found', component: NotFoundComponent},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
   {path: '**', redirectTo: '', pathMatch: 'full'},
 
