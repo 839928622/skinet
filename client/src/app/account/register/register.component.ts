a928351ac2f3bffb57f00e724aa255643e32bd88
alex <839928622@qq.com>
2020-11-20 17:58:19 +0800

    client register form and validation（async validation included）

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AsyncValidatorFn, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { of, timer } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { IUserRegister } from 'src/app/shared/models/userRegister';
+import { AccountService } from '../account.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +12,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+registerForm: FormGroup;
+userToLogin: IUserRegister;
+errors: string[];
+  constructor(private formBuider: FormBuilder,
+              private accountService: AccountService,
+              private router: Router, ) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();
   }
 
+ createRegisterForm(): void {
+   this.registerForm = this.formBuider.group({
+     displayName: [null, [Validators.required]],
+     email: [null, [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
+     password: [null, [Validators.required, Validators.pattern('^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$')],
+                      [this.validateEmailNotTaken()]] // async valitation will be called when sync validation are all passed above
+   });
+ }
+
+ onRegisterFormSubmit(): void {
+    if (this.registerForm.valid) {
+      this.userToLogin =   Object.assign({}, this.registerForm.value);
+      this.accountService.register(this.userToLogin.displayName, this.userToLogin.email, this.userToLogin.password).subscribe( () => {
+        this.router.navigateByUrl('/shop');
+        console.log('user registerd');
+      }, error => {
+        console.log(error);
+        this.errors = error.errors;
+      });
+  }
+  }
+
+  validateEmailNotTaken(): AsyncValidatorFn{
+    return control => {
+      return timer(500).pipe(
+        switchMap( () => {
+          if (!control.value) {
+            return of(null);
+          }
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map(res => {
+              return res ? { emailExists: true } : null;
+            })
+          );
+        })
+      );
+    };
+  }
 }
+
+
