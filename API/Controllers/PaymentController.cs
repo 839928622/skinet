638cc63e24e6c24eb76af3b6ace43b538424463f
Parker <jwk839928622@outlook.com>
2020-11-25 20:54:10 +0800

    CreateOrUpdatePaymentIntent in Payment Controller

---
+++ API/Controllers/PaymentController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class PaymentController : BaseController
+    {
+        private readonly IPaymentService _paymentService;
+        private readonly IMapper _autoMapper;
+
+        public PaymentController(IPaymentService paymentService, IMapper autoMapper)
+        {
+            _paymentService = paymentService;
+            _autoMapper = autoMapper;
+        }
+
+        [Authorize]
+        [HttpPost("{basketId}")]
+        public async Task<ActionResult<CustomerBasketDto>> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            return Ok(await _paymentService.CreateOrUpdatePaymentIntent(basketId));
+        }
+    }
+}
