d1f4b5fc1c96b6a57fd8d5c7f509e6b51efc5480
Parker <jwk839928622@outlook.com>
2021-11-10 21:09:08 +0800

    the StartUp class is not required on NET 6 , then I can safely remove it. add global Using statements  feature introduced on NET 6

---
+++ API/GlobalUsings.cs
@@ -0,0 +1,18 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using API.Extensions;
+global using API.Helpers;
+global using API.Middleware;
+
+global using Infrastructure.Data;
+global using Infrastructure.Data.SeedData;
+global using Infrastructure.Identity;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.OpenApi.Models;
+
--- API/Program.cs
+++ API/Program.cs
@@ -1,68 +1,171 @@
-using System;
-using System.Reflection;
-using System.Threading.Tasks;
+using StackExchange.Redis;
 using Core.Entities.Identity;
-using Infrastructure.Data;
-using Infrastructure.Data.SeedData;
-using Infrastructure.Identity;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace API
+// add services to the container
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<StoreContext>(options =>
 {
-    public class Program
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddDbContext<AppIdentityDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("IdentityConnection"));
+});
+
+builder.Services.AddSingleton<IConnectionMultiplexer, ConnectionMultiplexer>(config =>
+{
+    var configuration = ConfigurationOptions.Parse(builder.Configuration
+        .GetConnectionString("Redis"), true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
+builder.Services.AddAutoMapper(typeof(MappingProfiles));
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddApplicationServices();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo() { Title = "Skinet API", Version = "v1" });
+    var securitySchema = new OpenApiSecurityScheme()
     {
-        public static async Task Main(string[] args)
+        Description = "JWT Auth Bearer Schema",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        Type = SecuritySchemeType.Http,
+        Reference = new OpenApiReference()
         {
-          var host =  CreateHostBuilder(args).Build();
-            // apply migrations or database if it does not exist
-            using var scope = host.Services.CreateScope();
-            var services = scope.ServiceProvider;
-            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-            try
-            {
-                var context = services.GetRequiredService<StoreContext>();
-                 await context.Database.MigrateAsync();
-                 // seed product data
-                 await StoreContextSeed.SeedAsync(context, loggerFactory);
-
-                 // seed user
-                 var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-                 var identityContext = services.GetRequiredService<AppIdentityDbContext>();
-                 await identityContext.Database.MigrateAsync();
-                 await AppIdentityDbContextSeed.SeedUserAsync(userManager);
-            }
-            catch (Exception e)
-            {
-                var logger = loggerFactory.CreateLogger<Program>();
-                logger.LogError(e,"An error occured during migration");
-                
-            }
-
-            await host.RunAsync();
+            Type = ReferenceType.SecurityScheme,
+            Id = JwtBearerDefaults.AuthenticationScheme,
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args).
-                ConfigureAppConfiguration((hostingContext, config) =>
+    };
+    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, securitySchema);
+    var securityRequirement = new OpenApiSecurityRequirement()
                 {
-                    var env = hostingContext.HostingEnvironment;
-                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
-                    config.AddEnvironmentVariables();
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    var assemblyName = typeof(Startup).GetTypeInfo().Assembly.FullName;
+                    {securitySchema, new List<string>() {JwtBearerDefaults.AuthenticationScheme} }
+                };
+    options.AddSecurityRequirement(securityRequirement);
+});
 
-                    // webBuilder.UseStartup<Startup>();
-                    webBuilder.UseStartup(assemblyName);
-                    
-                });
-    }
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", policy =>
+    {
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+    });
+});
+
+
+// configure the http request pipeline
+var app = builder.Build();
+app.UseMiddleware<ExceptionMiddleware>();
+// if a request comes into Aapi but we dont have an endpoint that match 
+//that particular request then we are going to hit this bit of middleware and
+// its going to redirect to ErrorController
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+app.UseHttpsRedirection();
+
+// In the new hosting model, the app.UseRouting() doesn't do any harm to leaveit in place, but it is 
+// something we can actually remove because this is implicityly enabled that we want to use routing
+// app.UseRouting(); 
+app.UseStaticFiles(); // wwwroot
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseSwagger();
+app.UseSwaggerUI(x =>
+{
+    x.SwaggerEndpoint("/swagger/v1/swagger.json", "SKiNet APi v1");
+});
+
+app.MapControllers();
+app.MapFallbackToController("Index","Fallback");
+//app.UseEndpoints(endpoints =>
+//{
+//    endpoints.MapControllers();
+//});
+
+
+
+// apply migrations or database if it does not exist
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+try
+{
+    var context = services.GetRequiredService<StoreContext>();
+    await context.Database.MigrateAsync();
+    // seed product data
+    await StoreContextSeed.SeedAsync(context, loggerFactory);
+
+    // seed user
+    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+    var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+    await identityContext.Database.MigrateAsync();
+    await AppIdentityDbContextSeed.SeedUserAsync(userManager);
 }
+catch (Exception e)
+{
+    var logger = loggerFactory.CreateLogger<Program>();
+    logger.LogError(e, "An error occured during migration");
+
+}
+
+await app.RunAsync();
+
+//namespace API
+//{
+//    public class Program
+//    {
+//        public static async Task Main(string[] args)
+//        {
+//          var host =  CreateHostBuilder(args).Build();
+//            // apply migrations or database if it does not exist
+//            using var scope = host.Services.CreateScope();
+//            var services = scope.ServiceProvider;
+//            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+//            try
+//            {
+//                var context = services.GetRequiredService<StoreContext>();
+//                 await context.Database.MigrateAsync();
+//                 // seed product data
+//                 await StoreContextSeed.SeedAsync(context, loggerFactory);
+
+//                 // seed user
+//                 var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+//                 var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+//                 await identityContext.Database.MigrateAsync();
+//                 await AppIdentityDbContextSeed.SeedUserAsync(userManager);
+//            }
+//            catch (Exception e)
+//            {
+//                var logger = loggerFactory.CreateLogger<Program>();
+//                logger.LogError(e,"An error occured during migration");
+
+//            }
+
+//            await host.RunAsync();
+//        }
+
+//        public static IHostBuilder CreateHostBuilder(string[] args) =>
+//            Host.CreateDefaultBuilder(args).
+//                ConfigureAppConfiguration((hostingContext, config) =>
+//                {
+//                    var env = hostingContext.HostingEnvironment;
+//                    config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+//                          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+//                    config.AddEnvironmentVariables();
+//                })
+//                .ConfigureWebHostDefaults(webBuilder =>
+//                {
+//                    var assemblyName = typeof(Startup).GetTypeInfo().Assembly.FullName;
+
+//                    // webBuilder.UseStartup<Startup>();
+//                    webBuilder.UseStartup(assemblyName);
+
+//                });
+//    }
+//}
